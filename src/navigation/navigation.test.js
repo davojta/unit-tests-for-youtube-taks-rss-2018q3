fa44d6590b90130fd1211f259eed878722cad408
Dzianis Sheka <dzianis.sheka@gmail.com>
2018-11-29 23:25:23 +0300

    tests: augment first test with dom logic

--- src/navigation/navigation.test.js
+++ src/navigation/navigation.test.js
@@ -1,7 +1,13 @@
 import Navigation from './navigation';
 
 describe('Navigation', () => {
-  it('should work', () => {
-    expect(Navigation.draw).toBeInstanceOf(Function);
+  it('.draw should wrap template with tag section.navitation', () => {
+    // prepare - Arrange 
+
+    // action  - Act
+    Navigation.draw();
+
+    // check - Assert
+    expect(document.querySelector('body>section.navigation')).not.toBe('');
   });
 });
