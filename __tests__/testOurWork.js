3f97bae2a582a2529c60e42d99709814f6d40510
Dzianis Sheka <dzianis.sheka@gmail.com>
2018-11-29 23:50:06 +0300

    tests: add expanded example how we can test small function for ourself

--- __tests__/testOurWork.js
+++ __tests__/testOurWork.js
@@ -1,5 +1,39 @@
 
 
+const someConfig = {
+    red: 1,
+    black: 2
+}
+const colorConfig = {
+    1: 'red',
+    2: 'black'
+};
+
+const items = [
+    {
+        id: 1,
+        color: 1,
+    },
+    {
+        id: 2,
+        color: 2,
+    },
+    {
+        id: 3,
+        color: 3
+    }
+]
+
+function ourCompleFunction(items, config) {
+    return items
+    .filter(({ id, color }) => {
+        // console.log('id, color', id, color, config[color]);
+        return config[color] === 'red';
+    })
+    .map(({ id }) => id);      
+}
+
+
 describe('Very fast start to run the program', () => {
     it('insert some data into the dom', () => {
         // arrange
@@ -8,5 +42,10 @@ describe('Very fast start to run the program', () => {
         // assert
         const buttonEl = document.querySelector('#button');
         expect(buttonEl).not.toBe(null);
-    })
+    });
+    it('ourCompleFunction', () => {
+        const targetIds = ourCompleFunction(items, colorConfig);
+
+        expect(targetIds).toEqual([1])
+    });
 })
